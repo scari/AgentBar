ea45c9e99dced1c21489da8634d7a5cb90663949
Aaron.hj(아론) <aaron.hj@kakaostyle.com>
2026-03-09 15:24:46 +0900

    Adjust compact status bar row spacing

--- AgentBar/Views/StatusBar/StatusBarDisplayPlanner.swift
+++ AgentBar/Views/StatusBar/StatusBarDisplayPlanner.swift
@@ -2,9 +2,9 @@ import Foundation
 import CoreGraphics
 
 enum StatusBarDisplayPlanner {
-    static let visibleRowCount = 3
-    static let rowHeight: CGFloat = 6
-    static let rowSpacing: CGFloat = 1
+    static let maximumVisibleRowCount = 3
+    static let compactVisibleRowCount = 2
+    static let standardRowSpacing: CGFloat = 1
     static let viewportHeight: CGFloat = 20
 
     static let topPriorityHoldSeconds: TimeInterval = 8
@@ -30,11 +30,52 @@ enum StatusBarDisplayPlanner {
     }
 
     static func maxScrollIndex(for rankedServices: [UsageData]) -> Int {
-        max(0, rankedServices.count - visibleRowCount)
+        max(0, rankedServices.count - visibleRowCount(forServiceCount: rankedServices.count))
+    }
+
+    static func visibleRowCount(forServiceCount serviceCount: Int) -> Int {
+        guard serviceCount > 0 else { return maximumVisibleRowCount }
+        return serviceCount <= compactVisibleRowCount
+            ? compactVisibleRowCount
+            : maximumVisibleRowCount
+    }
+
+    static func rowHeight(forServiceCount serviceCount: Int) -> CGFloat {
+        if usesCompactLayout(forServiceCount: serviceCount) {
+            return compactRowHeight
+        }
+
+        let rowCount = CGFloat(visibleRowCount(forServiceCount: serviceCount))
+        let totalSpacing = standardRowSpacing * max(0, rowCount - 1)
+        return (viewportHeight - totalSpacing) / rowCount
+    }
+
+    static func rowSpacing(forServiceCount serviceCount: Int) -> CGFloat {
+        switch serviceCount {
+        case 2:
+            return (viewportHeight - (rowHeight(forServiceCount: serviceCount) * 2)) / 3
+        case 1:
+            return 0
+        default:
+            return standardRowSpacing
+        }
+    }
+
+    static func centersRowsVertically(forServiceCount serviceCount: Int) -> Bool {
+        usesCompactLayout(forServiceCount: serviceCount)
     }
 
     private static func usageScore(_ data: UsageData) -> Double {
         let weekly = data.weeklyUsage?.percentage ?? 0
         return max(data.fiveHourUsage.percentage, weekly)
+    }
+
+    private static var compactRowHeight: CGFloat {
+        let fullCompactRowHeight = (viewportHeight - standardRowSpacing) / CGFloat(compactVisibleRowCount)
+        return fullCompactRowHeight * 0.8
+    }
+
+    private static func usesCompactLayout(forServiceCount serviceCount: Int) -> Bool {
+        serviceCount > 0 && serviceCount <= compactVisibleRowCount
     }
 }
